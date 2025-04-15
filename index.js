390eb36596f7fb83267d3ec5adf9ccc25236dd31
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2025-04-15 14:28:18 -0300

    add nodemon

--- index.js
+++ index.js
@@ -10,12 +10,33 @@ const server = express();
 //   });
 // });
 
-server.get("/curso/:id", (req, res) => {
-  const id = req.params.id;
+const cursos = [
+  {
+    name: "HTML e CSS",
+    description: "Aprenda HTML e CSS do zero",
+    duration: "2 meses",
+  },
+  {
+    name: "JavaScript",
+    description: "Aprenda JavaScript do zero",
+    duration: "3 meses",
+  },
+  {
+    name: "Java",
+    description: "Aprenda Java do zero",
+    duration: "4 meses",
+  },
+  {
+    name: "Python",
+    description: "Aprenda Python do zero",
+    duration: "5 meses",
+  },
+];
 
-  return res.json({
-    curso: `Id do curso: ${id}`,
-  });
+server.get("/curso/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index]);
 });
 
 server.listen(3000);
