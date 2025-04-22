14db6458df3d00bbddee55f6de6e6816b71845b9
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2025-04-22 18:42:42 -0300

    working with middlewares in nodejs

--- index.js
+++ index.js
@@ -4,62 +4,72 @@ const server = express();
 
 server.use(express.json());
 
-// server.get("/curso", (req, res) => {
-//   const nome = req.query.nome;
-
-//   return res.json({
-//     curso: `Aprendendo ${nome}`,
-//   });
-// });
-
 const cursos = ["HTML e CSS", "JavaScript", "Java", "Python"];
 
-//read
-server.get("/cursos3", (req, res) => {
+//Middleware Global
+server.use((req, res, next) => {
+  console.log(`URL CHAMADA: ${req.url}`);
+
+  return next();
+});
+
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ èrror: "Nome do curso é obrigatório" });
+  }
+
+  return next();
+}
+
+function checkIndexCurso(req, res, next) {
+  //verificando se tem esse index
+  const curso = cursos[req.params.index];
+
+  if (!curso) {
+    return res.status(400).json({ error: "O curso não existe" });
+  }
+
+  req.curso = curso;
+
+  return next();
+}
+
+//READ
+server.get("/cursos", (req, res) => {
   return res.json(cursos);
 });
 
-//create
+server.get("/cursos/:index", checkIndexCurso, (req, res) => {
+  return res.json(req.curso);
+});
 
-server.post("/cursos", (req, res) => {
-  const { name, description, duration } = req.body;
+//CREATE
 
-  cursos.push({
-    name: name,
-    description: description,
-    duration: duration,
-  });
+server.post("/cursos", checkCurso, (req, res) => {
+  const { name } = req.body;
+
+  cursos.push(name);
 
   return res.json(cursos);
 });
 
-//update
-server.put("/cursos/:index", (req, res) => {
+//UPDATE
+server.put("/cursos/:index", checkCurso, checkIndexCurso, (req, res) => {
   const { index } = req.params;
-  const { name, description, duration } = req.body;
+  const { name } = req.body;
 
-  cursos[index] = {
-    name: name,
-    description: description,
-    duration: duration,
-  };
+  cursos[index] = name;
 
   return res.json(cursos);
 });
 
-//delete
-server.delete("/cursos/:index", (req, res) => {
+//DELETE
+server.delete("/cursos/:index", checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   cursos.splice(index, 1);
 
   return res.send();
-});
-
-server.get("/cursos/:index", (req, res) => {
-  const { index } = req.params;
-
-  return res.json(cursos[index]);
 });
 
 server.listen(3000);
